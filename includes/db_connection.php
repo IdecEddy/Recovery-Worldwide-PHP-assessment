e400f5aa8dfa7a4862f78e04a305b6039b97ba1b
ideceddy <edwingmundo2@gmail.com>
2016-04-07 10:21:52 -0400

    added db_connection.php a file that holds the info for db login.

---
+++ includes/db_connection.php
@@ -0,0 +1,8 @@
+<?php
+
+    defined ('DB_HOST') ? null : define('DB_HOST', "");
+    defined ('DB_USER') ? null : define('DB_USER', "");
+    defined ('DB_PASS') ? null : define('DB_PASS', "");
+    defined ('DB_NAME') ? null : define('DB_NAME', "");
+    
+?>
